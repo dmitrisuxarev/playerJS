bf174b636cc4ea9eaa462792ce76dec4d95ae011
dmitrisuxarev <dmitrisuxarev@gmail.com>
2023-02-10 22:04:18 +0300

    поправил дизайн

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ document.addEventListener("DOMContentLoaded",()=>{
 let playButton = document.querySelector(".play")
 let textButton = document.querySelector(".audio-text")
 let cover = document.querySelector(".cover")
-let songText = document.querySelector(".song-text")
+let playerWindow = document.querySelector(".player-window")
 
 
 playButton.addEventListener("click",(e)=>{
-    if(cover.classList.contains("cover-up")){
+    if(cover.classList.contains("cover-up")){ // если открыт текст и нажимаем кнопку играть\пауза
         cover.classList.toggle("cover-up-stop")
     }
     else{
-       cover.classList.toggle("cover-play") 
+       cover.classList.toggle("cover-play") //вкл\выкл проигрывание
     }
     
 })
 textButton.addEventListener("click",()=>{
-    if(cover.classList.contains("cover-up-stop")){
+    if(cover.classList.contains("cover-up-stop")){ //текст отключаем но оставляем состояние играть\пауза
         cover.classList.toggle("cover-up-stop")
         cover.classList.toggle("cover-play") 
     }
-    songText.classList.toggle("song-text-open")
+    playerWindow.classList.toggle("player-window-open") //открыть закрыть текст
     cover.classList.toggle("cover-up")
 })
 
